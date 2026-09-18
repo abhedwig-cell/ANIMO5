7602c29ef3fa6450d143d3381e55b915bb6380ff
abhedwig-cell <abhedwig@gmail.com>
2026-09-18 19:27:27 +0200

    repair(ANIMO-STATEQ11): compile KT12 interval-runtime dependency

--- tests/stateq11/run_stateq11_tests.sh
+++ tests/stateq11/run_stateq11_tests.sh
@@ -8,6 +8,7 @@ gfortran -std=f2008 -Wall -Wextra -Werror -ffp-contract=off -fno-fast-math -J . 
   "$root/prototype/kt02/runtime/mod_transient_time.f90" \
   "$root/prototype/kt02/runtime/mod_transient_contracts.f90" \
   "$root/prototype/kt02/runtime/mod_transient_transactions.f90" \
+  "$root/prototype/kt02/runtime/mod_transient_interval_runtime.f90" \
   "$root/prototype/ubforce01/mod_animo_tcd042_upper_solute_load_contract.f90" \
   "$root/prototype/ubforce02/mod_animo_tcd042_upper_solute_load_resolver.f90" \
   "$root/prototype/boundq01/mod_animo_static_boundary_chemistry_adapter.f90" \
